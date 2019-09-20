c53972fad72f510d618eaad4da7429c80aea0946
Jody Clements <jody@neomorphic.co.uk>
2019-09-20 16:59:07 -0400

    Adds eslint and prettier for formatting.

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class HeatMap extends React.Component {
+  componentDidMount() {
+    // set up the target div.
+  }
+
+  render() {
+    return <p>HeatMap</p>;
+  }
+}
