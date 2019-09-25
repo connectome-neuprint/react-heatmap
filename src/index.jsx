7b5368f846b8b6558ae444ac51fd3ea256c1d1e8
Jody Clements <jody@neomorphic.co.uk>
2019-09-25 09:18:25 -0400

    First working version of react component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import HeatMapObject from "@neuprint/heatmap";
+import PropTypes from 'prop-types';
 
 export default class HeatMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.heatMapRef = React.createRef();
+  }
+
   componentDidMount() {
-    // set up the target div.
+    const { data, yLabels, xLabels, height, width } = this.props;
+    const heatmap = new HeatMapObject({
+      height: height || this.heatMapRef.current.clientHeight,
+      width: width || this.heatMapRef.current.clientWidth,
+    });
+    heatmap.setYLabels(yLabels).setXLabels(xLabels).setData(data).render(this.heatMapRef.current);
+  }
+
+  componentDidUpdate() {
+    const { data, yLabels, xLabels } = this.props;
+    const heatmap = new HeatMapObject({
+      height: this.heatMapRef.current.clientHeight,
+      width: this.heatMapRef.current.clientWidth,
+    });
+    heatmap.setYLabels(yLabels).setXLabels(xLabels).setData(data).render(this.heatMapRef.current);
   }
 
   render() {
-    return <p>HeatMap</p>;
+    return (
+      <div ref={this.heatMapRef} id="heatmap" />
+    );
   }
 }
+
+HeatMap.defaultProps = {
+  height: null,
+  width: null,
+};
+
+HeatMap.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  xLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  yLabels: PropTypes.arrayOf(PropTypes.string).isRequired
+};
